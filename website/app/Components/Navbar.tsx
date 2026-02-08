8cd2aeaf78b18619388ef6be75de7a49cfcb2a84
alikansoh <alikanso.dev@gmail.com>
2026-02-08 12:38:30 +0000

    new updates

--- website/app/Components/Navbar.tsx
+++ website/app/Components/Navbar.tsx
@@ -963,7 +963,7 @@ export default function Navbar() {
       </nav>
 
       {/* Spacer to prevent content behind fixed header */}
-      <div className="h-12 lg:h-12" aria-hidden="true" />
+      <div className="h-22 lg:h-22" aria-hidden="true" />
     </>
   );
 }
